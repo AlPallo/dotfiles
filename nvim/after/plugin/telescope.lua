ca5fc84372a87c7215feea416021d77985577c8e
Alex <pallotaw@gmail.com>
2025-08-22 17:12:19 +0000

    vim style visual select in tmux copy mode, change git file search binding

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -17,7 +17,7 @@ require("telescope").setup({
     },
 })
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
