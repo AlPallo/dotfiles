ffe987b8808a18f8795a262ac1305cd173f0b12a
Alex <pallotaw@gmail.com>
2025-09-30 08:32:35 +0000

    add inital fish config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+set -U __fish_git_prompt_show_informative_status 1
+set -U __fish_git_prompt_char_stagedstate "+"
+set -U __fish_git_prompt_color_stagedstate green
+set -U __fish_git_prompt_char_dirtystate "*"
+set -U __fish_git_prompt_color_dirtystate red
+set -U __fish_git_prompt_char_cleanstate ""
+set -U __fish_git_prompt_char_conflictedstate "!"
+set -U __fish_git_prompt_color_conflictedstate red
+set -U __fish_git_prompt_color_branch magenta
+set -U __fish_git_prompt_char_untrackedfiles "…"
+set -U fish_color_cwd white
+end
